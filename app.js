646a78f8dfb4dd42003419b38cd7a281cf626c4b
FaBLee <58670304+fablee1@users.noreply.github.com>
2021-05-25 20:01:35 +0100

    ex5 done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // https://jsonplaceholder.typicode.com/users
 
 let fetchedData
+let lastData
 
 
 const generateCard = (user) => { 
@@ -27,8 +28,39 @@ const display = (data=fetchedData) => {
 
 const filter = (option, input) => {
   let filteredArray = fetchedData.filter(u => u[option].toLowerCase().includes(input.toLowerCase()))
+  lastData = filteredArray
   display(filteredArray)
 }
+
+const sortFunc = (user1, user2) => {
+  let name1 = user1.name.toUpperCase(); // ignore upper and lowercase
+  let name2 = user2.name.toUpperCase(); // ignore upper and lowercase
+  if (name1 < name2) {
+    return -1;
+  }
+  if (name1 > name2) {
+    return 1;
+  }
+  return 0;
+}
+
+const sortByName = () => {
+  const sorted = JSON.stringify(lastData) == JSON.stringify([...lastData].sort((a,b) => sortFunc(a, b)))
+
+  const svg = document.getElementById('svgSorted')
+
+  if(sorted) {
+    svg.innerHTML = `<path d="M12.96 7H9.028v-.691l2.579-3.72v-.054H9.098v-.867h3.785v.691l-2.567 3.72v.054h2.645V7z"/>
+    <path fill-rule="evenodd" d="M10.082 12.629 9.664 14H8.598l1.789-5.332h1.234L13.402 14h-1.12l-.419-1.371h-1.781zm1.57-.785L11 9.688h-.047l-.652 2.156h1.351z"/>
+    <path d="M4.5 2.5a.5.5 0 0 0-1 0v9.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L4.5 12.293V2.5z"/>`
+  } else {
+    svg.innerHTML = `<path fill-rule="evenodd" d="M10.082 5.629 9.664 7H8.598l1.789-5.332h1.234L13.402 7h-1.12l-.419-1.371h-1.781zm1.57-.785L11 2.687h-.047l-.652 2.157h1.351z"/>
+    <path d="M12.96 14H9.028v-.691l2.579-3.72v-.054H9.098v-.867h3.785v.691l-2.567 3.72v.054h2.645V14zM4.5 2.5a.5.5 0 0 0-1 0v9.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L4.5 12.293V2.5z"/>` 
+  }
+
+  const sortedArray = sorted ? lastData.reverse() : lastData.sort((a,b) => sortFunc(a, b))
+  display(sortedArray)
+} 
 
 
 window.onload = function() {
@@ -36,6 +68,7 @@ window.onload = function() {
     .then(response => response.json())
     .then(data => {
       fetchedData = data
+      lastData = data
       display()
     })
     .catch(err => console.log(err))
@@ -48,6 +81,10 @@ window.onload = function() {
 
       filter(option, input.value)
     })
+
+    // addeventlistener to sort button
+    const sortBtn = document.getElementById('sort')
+    sortBtn.addEventListener('click', () => sortByName())
 }
 
 const extractNames = () =>  console.log(fetchedData.map(el => el.name).join(', '))
