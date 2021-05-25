1ff38bf95f97e94744d5b1abbf07862b404e99b7
FaBLee <58670304+fablee1@users.noreply.github.com>
2021-05-25 19:00:04 +0100

    ex2 done

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // https://jsonplaceholder.typicode.com/users
 
+let fetchedData
+
+
 const generateCard = (user) => { 
     const col = document.createElement('div')
     col.classList = 'col col-md-4 col-lg-3'
@@ -8,26 +11,41 @@ const generateCard = (user) => {
       ${user.name}
     </div>
     <ul class="list-group list-group-flush">
+      <li class="list-group-item">Username: ${user.username}</li>
       <li class="list-group-item">Email: ${user.email}</li>
       <li class="list-group-item">City: ${user.address.city}</li>
       <li class="list-group-item">Site: ${user.website}</li>
     </ul>
   </div>` 
   return col.outerHTML
-    
-
-
 }
 
-const display = (data) => { 
+const display = (data=fetchedData) => { 
     let userCards = data.map(el => generateCard(el))
     document.getElementById('cards').innerHTML = userCards.join('')
 }
 
-window.onload = function(){
+const filter = (option, input) => {
+  let filteredArray = fetchedData.filter(u => u[option].toLowerCase().includes(input.toLowerCase()))
+  display(filteredArray)
+}
 
+
+window.onload = function() {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then(data => display(data))
+    .then(data => {
+      fetchedData = data
+      display()
+    })
     .catch(err => console.log(err))
+
+
+    // addeventlistener to input of filter 
+    const input = document.getElementById('filterInput')
+    input.addEventListener('input', () => {
+      const option = document.getElementById('filterOption').value
+
+      filter(option, input.value)
+    })
 }
